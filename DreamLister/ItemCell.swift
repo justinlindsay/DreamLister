dcd008e79e894222a0f4c5cb2b56a0c6548f368d
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-20 17:11:28 -0400

    created fetchresult controller. Created fetch attempt function. Implement methods.

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by Justin Lindsay on 7/20/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+    
+}
