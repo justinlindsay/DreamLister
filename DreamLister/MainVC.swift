34fb499d2eee824b83cd16b2becafa1b5f038fc7
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-20 22:08:11 -0400

    Implimented save functionality

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -85,6 +85,7 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         
         let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
         
+        controller.delegate = self
         
         self.controller = controller
         
